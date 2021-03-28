ef2af91be531ed94aca253e2b292d9fdf5ac6b37
John O'Hara <johara@redhat.com>
2021-03-28 08:06:46 +0100

    Add logging for MonitorFactory

--- raspberry-pi-agent/src/main/java/me/johara/picocli/util/MonitorFactory.java
+++ raspberry-pi-agent/src/main/java/me/johara/picocli/util/MonitorFactory.java
@@ -3,10 +3,16 @@ package me.johara.picocli.util;
 import me.johara.picocli.monitor.ElectricityMonitor;
 import me.johara.picocli.monitor.PinStateMonitor;
 import me.johara.picocli.monitor.WaterMonitor;
+import org.jboss.logging.Logger;
 
 public class MonitorFactory {
 
+    private static final Logger logger = Logger.getLogger(MonitorFactory.class);
+
     public static PinStateMonitor buildMonitor(String monitorName, String pinName, int debounce, String trigger, boolean debug){
+
+        logger.infof("Building monitor for utility: %s\n", monitorName);
+
         switch (monitorName.toUpperCase()){
             case "WATER":
                 return new WaterMonitor(pinName, debounce, trigger,debug);
